ecd251ab82a403b3c2319119836706fe0ab39938
Keti Yohannes <ketiyohannes@gmail.com>
2023-04-29 12:14:37 +0300

    first iteration

---
+++ ecommerce/src/components/Footer.jsx
@@ -0,0 +1,41 @@
+import React from 'react'
+
+function Footer() {
+  return (
+    <div className="h-screen">
+        <div className=" h-[50%] flex items-center justify-center font-semibold text-[50px]">
+          SOME FOOTER TEXT GOES HERE
+        </div>
+        <div className="h-[50%] bg-slate-800 text-white flex items-center justify-center space-x-40 text-xl">
+          <div>
+            <p>Company Info</p>
+            <p>Some info here</p>
+            <p>Some info here</p>
+            <p>Some info here</p>
+            <p>Some info here</p>
+            <p>Some info here</p>
+          </div>
+
+          <div>
+            <p>Company Info</p>
+            <p>Some info here</p>
+            <p>Some info here</p>
+            <p>Some info here</p>
+            <p>Some info here</p>
+            <p>Some info here</p>
+          </div>
+
+          <div>
+            <p>Company Info</p>
+            <p>Some info here</p>
+            <p>Some info here</p>
+            <p>Some info here</p>
+            <p>Some info here</p>
+            <p>Some info here</p>
+          </div>
+        </div>
+      </div>
+  )
+}
+
+export default Footer
